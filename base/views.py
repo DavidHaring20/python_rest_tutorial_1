b56e5842ace468dde374f12cb2fa11c69744fe45
DavidHaring20 <davidharingri@gmail.com>
2022-11-04 18:20:23 +0100

    1:05:25

--- base/views.py
+++ base/views.py
@@ -1,16 +1,32 @@
-from django.http import JsonResponse
+from django.db.models import Q
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Advocate
+from .serializers import AdvocateSerializer
 
 # Create your views here.
 
-
+@api_view(['GET'])
 def endpoints(request):
     data = ['/advocates', 'advocates/:username']
-    return JsonResponse(data, safe=False)
+    return Response(data)
 
+@api_view(['GET'])
 def advocate_list(request):
-    data = ['Dennis', 'Tadas', 'Nax']
-    return JsonResponse(data, safe=False)
+    query = request.GET.get('query')
+    
+    if query == None:
+        query = ''
 
+
+    advocates = Advocate.objects.filter(Q(username__icontains=query) | Q(bio__icontains=query))
+    serializer = AdvocateSerializer(advocates, many=True) 
+    return Response(serializer.data)
+
+@api_view(['GET'])
 def advocate_detail(request, username):
-    data = username
-    return JsonResponse(data, safe=False)
+    advocate = Advocate.objects.get(username=username)
+    serializer = AdvocateSerializer(advocate, many=False)
+    return Response(serializer.data)
